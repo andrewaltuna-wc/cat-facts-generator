2307e0a7699762bc09e942330847f2cb24fa2407
andrew.altuna <andrew.altuna@whitecloak.com>
2023-01-17 18:33:37 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:cat_facts_generator/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,150 @@
+import 'dart:convert';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'package:shimmer/shimmer.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late double _deviceWidth;
+  bool _disableButton = false;
+  bool _isLoading = false;
+  // String catImage = "";
+  NetworkImage? catImage;
+  String catFact = "Click on generate to view a random cat fact!";
+  String? catFactId;
+
+  void generateContent() async {
+    // Image
+    // final int imageId = Random().nextInt(10);
+    final Uri imageUrl = Uri.https("cataas.com", "cat", {'json': 'true'});
+    final imageResponse = await get(imageUrl);
+    final imageJson = json.decode(imageResponse.body);
+
+    // Fact
+    final int factId = Random().nextInt(91);
+    final Uri factUrl =
+        Uri.https("meowfacts.herokuapp.com", "", {'id': '$factId'});
+    final factResponse = await get(factUrl);
+    final factJson = json.decode(factResponse.body);
+
+    setState(() {
+      _disableButton = false;
+      _isLoading = false;
+      catFact = factJson['data'][0];
+      catFactId = factId.toString();
+      catImage = NetworkImage("https://cataas.com${imageJson['url']}");
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _deviceWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(title: const Text("Cat Facts Generator")),
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        color: const Color.fromRGBO(25, 39, 52, 1),
+        child: SafeArea(
+          child: Center(
+            child: AnimatedOpacity(
+              opacity: _isLoading ? 0.8 : 1,
+              duration: const Duration(milliseconds: 200),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  if (catImage != null)
+                    Stack(children: [
+                      Shimmer.fromColors(
+                        baseColor: Colors.white.withOpacity(0.25),
+                        highlightColor: Colors.white.withOpacity(0.4),
+                        child: imageHolderWidget(),
+                      ),
+                      imageHolderWidget(catImage)
+                    ]),
+                  const SizedBox(height: 10),
+                  factWidget(),
+                  const SizedBox(height: 10),
+                  ElevatedButton(
+                    onPressed: _disableButton
+                        ? null
+                        : () {
+                            generateContent();
+                            setState(() {
+                              _disableButton = true;
+                              _isLoading = true;
+                            });
+                          },
+                    // style: ElevatedButton.styleFrom(
+                    //     minimumSize: Size.fromHeight(30)),
+                    child: const Text("GENERATE"),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget imageHolderWidget([NetworkImage? image]) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: Container(
+        height: 200,
+        decoration: image == null
+            ? BoxDecoration(
+                color: Colors.white.withOpacity(0.2),
+              )
+            : BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: image,
+                ),
+              ),
+      ),
+    );
+  }
+
+  Widget factWidget() {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        children: [
+          if (catFactId != null) ...[
+            Text(
+              "Cat Fact #$catFactId",
+              style: const TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.w800,
+                fontSize: 20,
+              ),
+            ),
+            const SizedBox(height: 10),
+          ],
+          Text(
+            catFact,
+            style: const TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w300,
+              fontSize: 18,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+}
